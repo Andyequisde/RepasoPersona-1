7afd9e5efea1d1c9facaf8f7cebefa8f33b4b0ef
Andyequisde <andyrevollopro@gmail.com>
2022-03-31 15:57:37 -0300

    avances xd
    a

---
+++ EnteConSaldo.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace RepasoPersona.Test
+{
+    public class EnteConSaldo
+    {
+        public int Saldo { get; set; }
+
+        public void Debitar(double monto)
+        {
+            if (monto <= 0)
+                throw new ArgumentException("El monto tiene que ser mayor a cero.");
+
+            if (monto > Efectivo)
+                throw new InvalidOperationException("El monto supera al efectivo.");
+            Efectivo -= monto;
+        }
+        public void Acreditar(double monto)
+        {
+            if (monto <= 0)
+                throw new ArgumentException("El monto tiene que ser mayor a cero.");
+            Efectivo += monto;
+        }
+    }
+}
--- RepasoPersona.Core/Persona.cs
+++ RepasoPersona.Core/Persona.cs
@@ -14,21 +14,5 @@ namespace RepasoPersona.Core
             Apellido = apellido;
             Efectivo = efectivo;
         } 
-
-        public void Debitar(double monto)
-        {
-            if (monto <= 0)
-                throw new ArgumentException("El monto tiene que ser mayor a cero.");
-
-            if (monto > Efectivo)
-                throw new InvalidOperationException("El monto supera al efectivo.");
-            Efectivo -= monto;
-        }
-        public void Acreditar(double monto)
-        {
-            if (monto <= 0)
-                throw new ArgumentException("El monto tiene que ser mayor a cero.");
-            Efectivo += monto;
-        }
     }
 }
